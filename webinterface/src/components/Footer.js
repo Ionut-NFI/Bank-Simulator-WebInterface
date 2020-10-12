b3a4ecf4fe906d48992995a9f9a699bfabe4365a
Neagu Florin Ionut <neaguflorinionut@yahoo.com>
2020-10-12 11:46:03 +0300

    init Header and Footer in web interface

---
+++ webinterface/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        THIS IS FOOTER
+      </header>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ webinterface/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        THIS IS HEADER
+      </header>
+    </div>
+  );
+}
+
+export default Header;
